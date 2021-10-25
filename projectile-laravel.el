97f1ffb7be88ce6d948bf97411ade7d1651dfd63
strikerlulu <strikerlulu7@gmail.com>
2021-10-25 22:22:35 +0530

    Add package summary

--- projectile-laravel.el
+++ projectile-laravel.el
@@ -1,3 +1,40 @@
+;;; projectile-laravel.el --- Minor mode for Laravel projects based on projectile-mode
+
+;; Copyright (C) 2020 Strikerlulu
+
+;; Author:            Strikerulu <strikerlulu@gmail.com>
+;; URL:               https://github.com/strikerlulu/projectile-laravel
+;; Version:           0.10.0
+;; Keywords:          laravel, projectile
+;; Package-Requires:  ((emacs "24.3") (projectile "0.12.0") (inflections "1.1"))
+
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;;
+;; To start it for the laravel projects:
+;;
+;;    (projectile-laravel-global-mode)
+;;
+
+;;; Code:
 (eval-when-compile (require 'cl-lib))
 (require 'projectile)
 (require 'inflections)
